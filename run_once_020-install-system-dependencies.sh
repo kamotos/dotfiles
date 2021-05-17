a2ec6adb03e547857d7ba63aa8d4639744df11b0
Anas ZAHIM <anas.zahim@treezor.com>
2021-05-17 20:27:46 +0200

    fzf

--- run_once_020-install-system-dependencies.sh
+++ run_once_020-install-system-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-OS_DEPENDENCIES="libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git python3-dev flameshot sqlite3 libsqlite3-dev i3lock-fancy pulseaudio-utils xbacklight build-essential"
+OS_DEPENDENCIES="fzf libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git python3-dev flameshot sqlite3 libsqlite3-dev i3lock-fancy pulseaudio-utils xbacklight build-essential"
 
 SUDO=""
 if type sudo
