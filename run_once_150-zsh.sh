c83cc1498b5012545b509b639ff6745e12b770b2
Anas ZAHIM <anas.zahim@deliveroo.co.uk>
2021-06-04 09:47:19 +0200

    Removing pyenv from .zshrc

--- run_once_150-zsh.sh
+++ run_once_150-zsh.sh
@@ -4,5 +4,5 @@ set -x
 
 # Plugins
 ## zsh-autosuggestions
-export ZSH_AUTOSUGGESTIONS_DIR="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+export ZSH_AUTOSUGGESTIONS_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_AUTOSUGGESTIONS_DIR}" || true
