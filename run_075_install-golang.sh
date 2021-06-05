cfc45a16e8ea349b4dc04d2b34932236d484ed7d
Anas ZAHIM <anas.zahim@deliveroo.co.uk>
2021-06-05 10:03:37 +0200

    go installation script

---
+++ run_075_install-golang.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+INSTALLED_VERSION=$(go env GOVERSION)
+GOLANG_LATEST_VERSION=$(curl https://golang.org/VERSION?m=text)
+
+if [ "$INSTALLED_VERSION" = "$GOLANG_LATEST_VERSION" ] 
+then
+	exit 0
+fi
+
+url=https://golang.org/dl/${GOLANG_LATEST_VERSION}.darwin-amd64.pkg
+
+curl -L -o /tmp/golang-installer.pkg $url
+
+sudo mv /usr/local/go /usr/local/${GOLANG_LATEST_VERSION}
+
+open /tmp/golang-installer.pkg
+
+
+
