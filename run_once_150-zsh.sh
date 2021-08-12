991cb0717d59593474897bea9230ebe4130cde9b
Zahim Anas <anas.zahim@deliveroo.co.uk>
2021-08-12 20:00:46 +0200

    Installin evalcache plugin

--- run_once_150-zsh.sh
+++ run_once_150-zsh.sh
@@ -6,3 +6,5 @@ set -x
 ## zsh-autosuggestions
 export ZSH_AUTOSUGGESTIONS_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_AUTOSUGGESTIONS_DIR}" || true
+
+git clone https://github.com/mroth/evalcache ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/evalcache || true
