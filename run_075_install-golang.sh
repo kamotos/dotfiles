8c53114134983721a65909bc92af40197d6c5fd2
Anas ZAHIM <zanass0@gmail.com>
2022-09-08 16:45:46 +0200

    golang installation on fresh laptop

--- run_075_install-golang.sh
+++ run_075_install-golang.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -euxo pipefail
 
-INSTALLED_VERSION=$(go env GOVERSION)
+INSTALLED_VERSION=$(go env GOVERSION || echo "")
 GOLANG_LATEST_VERSION=$(curl https://go.dev/VERSION?m=text)
 
 if [ "$INSTALLED_VERSION" = "$GOLANG_LATEST_VERSION" ] 
@@ -9,13 +9,24 @@ then
 	exit 0
 fi
 
-url=https://golang.org/dl/${GOLANG_LATEST_VERSION}.darwin-amd64.pkg
+# Nothing to move during first installation
+if [ "$INSTALLED_VERSION" != "" ]
+then
+	sudo mv /usr/local/go /usr/local/${INSTALLED_VERSION}
+fi
 
-curl -L -o /tmp/golang-installer.pkg $url
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	url=https://golang.org/dl/${GOLANG_LATEST_VERSION}.darwin-amd64.pkg
+	curl -L -o /tmp/golang-installer.pkg $url
+	open /tmp/golang-installer.pkg
+else 	
+	FILENAME=${GOLANG_LATEST_VERSION}.linux-amd64.tar.gz
+	url=https://golang.org/dl/$FILENAME
+	curl -L -o /tmp/$FILENAME $url
+	sudo tar -C /usr/local -xzf /tmp/$FILENAME
+fi
 
-sudo mv /usr/local/go /usr/local/${INSTALLED_VERSION}
 
-open /tmp/golang-installer.pkg
 
 
 
