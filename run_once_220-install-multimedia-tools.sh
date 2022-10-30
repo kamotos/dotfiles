11565997659622579520e842273d2ebd008e3398
Anas ZAHIM <zanass0@gmail.com>
2022-10-30 13:04:10 +0100

    playerctl

--- run_once_220-install-multimedia-tools.sh
+++ run_once_220-install-multimedia-tools.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+
+install_playerctl() {
+	if [[ "$(type playerctl)" == "0" ]]; then
+		return 0
+	fi
+
+	curl -L -o /tmp/playerctl.deb https://github.com/altdesktop/playerctl/releases/download/v2.4.1/playerctl-2.4.1_amd64.deb
+
+	sudo dpkg -i /tmp/playerctl.deb
+}
+
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	type spotify || snap install spotify
+
+	install_playerctl
 fi
+
